852ca73cadf9c7c7821fe35132c85bb823831917
HipJaengYiCat <shee6185@gmail.com>
2023-04-14 06:45:23 +0000

    ãremove freeze function

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.models import resnet34, resnext50_32x4d
+from torchvision.models import resnet34, resnext50_32x4d, vgg19
 from efficientnet_pytorch import EfficientNet
 
 class BaseModel(nn.Module):
@@ -37,15 +37,11 @@ class EfficientNetB3(nn.Module):
     '''
     생성자 : 박승희
     '''
-    def __init__(self, num_classes, freeze = False):
+    def __init__(self, num_classes):
         super(EfficientNetB3, self).__init__()
         self.model = EfficientNet.from_pretrained('efficientnet-b3')
         self.num_ftrs = self.model._fc.in_features
         self.model._fc = nn.Linear(self.num_ftrs, num_classes)
-
-        if freeze :
-            for param in self.model.parameters():
-                param.requires_grad = False
 
     def forward(self, x):
         x = self.model(x)
@@ -55,15 +51,25 @@ class EfficientNetB4(nn.Module):
     '''
     생성자 : 박승희
     '''
-    def __init__(self, num_classes, freeze = False):
+    def __init__(self, num_classes):
         super(EfficientNetB4, self).__init__()
         self.model = EfficientNet.from_pretrained('efficientnet-b4')
         self.num_ftrs = self.model._fc.in_features
         self.model._fc = nn.Linear(self.num_ftrs, num_classes)
 
-        if freeze :
-            for param in self.model.parameters():
-                param.requires_grad = False
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+class EfficientNetB5(nn.Module):
+    '''
+    생성자 : 박승희
+    '''
+    def __init__(self, num_classes):
+        super(EfficientNetB5, self).__init__()
+        self.model = EfficientNet.from_pretrained('efficientnet-b5')
+        self.num_ftrs = self.model._fc.in_features
+        self.model._fc = nn.Linear(self.num_ftrs, num_classes)
 
     def forward(self, x):
         x = self.model(x)
@@ -75,16 +81,11 @@ class ResNet34(nn.Module):
     수정자 : 박승희
     pretrain된 resnet34를 가져와 
     '''
-    def __init__(self, num_classes, freeze = False):
-        super(EfficientNetB4, self).__init__()
+    def __init__(self, num_classes):
+        super(ResNet34, self).__init__()
         self.model = resnet34(pretrained=True)
         self.num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs, num_classes) # 18
-
-        if freeze :
-            for param in self.model.parameters():
-                param.requires_grad = False
-
 
     def forward(self, x):
         x = self.model(x)
@@ -95,16 +96,27 @@ class ResNext50(nn.Module):
     생성자 : 이다현
     수정자 : 박승희
     '''
-    def __init__(self, num_classes, freeze = False):
-        super(EfficientNetB4, self).__init__()
+    def __init__(self, num_classes):
+        super(ResNext50, self).__init__()
         self.model = resnext50_32x4d(pretrained=True)
         self.num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(self.num_ftrs, num_classes) # 18
-
-        if freeze :
-            for param in self.model.parameters():
-                param.requires_grad = False
-
+        
+    def forward(self, x):
+        x = self.model(x)
+        return x
+    
+class Vgg19(nn.Module):
+    '''
+    생성자 : 이상민
+    수정자 : 박승희
+    '''
+    def __init__(self, num_classes):
+        super(Vgg19, self).__init__()
+        self.model = vgg19(pretrained=True)
+        self.num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(self.num_ftrs, num_classes) # 18
+        
     def forward(self, x):
         x = self.model(x)
         return x

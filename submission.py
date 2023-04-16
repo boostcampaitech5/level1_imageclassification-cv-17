281d19b1edcf7f49e4fc75b57b98e96aadf41e9e
HipJaengYiCat <shee6185@gmail.com>
2023-04-16 15:20:10 +0000

    fix with torch.no_grad() -> for loader

--- submission.py
+++ submission.py
@@ -41,8 +41,9 @@ def submission(model, save_dir):
 
     # 모델이 테스트 데이터셋을 예측하고 결과를 저장합니다.
     all_predictions = []
-    for images in loader:
-        with torch.no_grad():
+#   for images in loader:
+    with torch.no_grad():
+        for images in loader:
             images = images.to(device)
             pred = model(images)
             pred = pred.argmax(dim=-1)
